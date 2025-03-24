12140dfeb91632c8ca8ab5c6f89b444cdf871149
Markus Fischer <fischer@mideasdesign.de>
2025-03-24 19:31:59 +0100

    resize the mobile cart

--- assets/js/template.js
+++ assets/js/template.js
@@ -17,24 +17,24 @@ function getDishesListTemplate(indexDishes) {
 
 function getCartTemplate(indexCart){
   return /* html */`
-    <div id="cart-item${[indexCart]}" class="grid grid-cols-5 gap-3 content-center"> 
-      <div class="content-center col-span-2">
-      <button id="trash${[indexCart]}" onclick="toTrash(${[indexCart]})" class="pr-5"><img src="./assets/icon/trash.svg" alt="delete dish" class="h-4 w-auto hover:cursor-pointer"></button>
+    <div id="cart-item${[indexCart]}" class="flex sm:grid sm:grid-cols-5 sm:gap-3 gap-1 content-center"> 
+      <div class="content-center sm:col-span-2">
+      <button id="trash${[indexCart]}" onclick="toTrash(${[indexCart]})" class="p-1 sm:pr-5"><img src="./assets/icon/trash.svg" alt="delete dish" class="h-3 w-auto hover:cursor-pointer"></button>
         <button onclick="amountMinus(${[indexCart]})" class="hover:cursor-pointer">-</button>
-        <span id="count${[indexCart]}" class="p-4">${cart[indexCart].amount}</span>
+        <span id="count${[indexCart]}" class="p-1">${cart[indexCart].amount}</span>
         <button onclick="amountPlus(${[indexCart]})" class="hover:cursor-pointer">+</button>
       </div>
-      <h4 class="p-2 col-span-2">${cart[indexCart].dish}</h4>
-      <p class="py-2 text-right"><span>${cart[indexCart].total.toFixed(2)}</span> €</p>
+      <h4 class="p-1 sm:col-span-2">${cart[indexCart].dish}</h4>
+      <p class="py-1 text-right"><span>${cart[indexCart].total.toFixed(2)}</span> €</p>
     </div>
   `
 };
 
 function getGrandTotalTemplate(grandTotal){
   return /* html */`
-    <div id="sum-container" class="grid grid-cols-3 gap-3 mt-5">
-      <h4 class="text-lg col-span-2">Lieferkosten:</h4><p class="text-xl text-right">${3.50.toFixed(2)} €</p>
-      <h4 class="text-lg col-span-2">Gesamtkosten:</h4><p class="text-xl text-right">${grandTotal.toFixed(2)} €</p>
+    <div id="sum-container" class="grid sm:grid-cols-3 sm:gap-3 mt-5">
+      <h4 class="text-m col-span-2">Lieferkosten:</h4><p class="text-m text-right">${3.50.toFixed(2)} €</p>
+      <h4 class="text-m col-span-2">Gesamtkosten:</h4><p class="text-m text-right">${grandTotal.toFixed(2)} €</p>
       <button onclick="orderDishes()" class="rounded-2xl text-m mt-4 uppercase text-white cart-btn w-full shadow-sm my-2 p-4 col-span-3 hover:cursor-pointer">jetzt bestellen</button>
     </div>
   `
